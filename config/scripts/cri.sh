61b4e8f11ed8b9f34f42c417460c65a7d8670b9f
Kenan Erdogan <kenanerdogan@gmail.com>
2025-12-16 12:52:35 +0100

    add readme and do refactor

--- config/scripts/cri.sh
+++ config/scripts/cri.sh
@@ -4,11 +4,18 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+: "${CONTAINER_REGISTRY_URL:?CONTAINER_REGISTRY_URL must be set}"
+
+if [[ ! "$CONTAINER_REGISTRY_URL" =~ ^https:// ]]; then
+  echo "Error: CONTAINER_REGISTRY_URL must start with https://"
+  exit 1
+fi
+
 mkdir -p /etc/containerd/certs.d/docker.io
 cat >/etc/containerd/certs.d/docker.io/hosts.toml <<EOL
 server = "https://docker.io"
 
-[host."http://harbor.devops1.pbm.sh/v2/proxy-docker.io"]
+[host."${CONTAINER_REGISTRY_URL}/v2/proxy-docker.io"]
   capabilities = ["pull", "resolve"]
   override_path = true
 EOL
@@ -17,7 +24,7 @@ mkdir -p /etc/containerd/certs.d/gcr.io
 cat >/etc/containerd/certs.d/gcr.io/hosts.toml <<EOL
 server = "https://gcr.io"
 
-[host."http://harbor.devops1.pbm.sh/v2/proxy-gcr.io"]
+[host."${CONTAINER_REGISTRY_URL}/v2/proxy-gcr.io"]
   capabilities = ["pull", "resolve"]
   override_path = true
 EOL
@@ -26,7 +33,7 @@ mkdir -p /etc/containerd/certs.d/ghcr.io
 cat >/etc/containerd/certs.d/ghcr.io/hosts.toml <<EOL
 server = "https://ghcr.io"
 
-[host."http://harbor.devops1.pbm.sh/v2/proxy-ghcr.io"]
+[host."${CONTAINER_REGISTRY_URL}/v2/proxy-ghcr.io"]
   capabilities = ["pull", "resolve"]
   override_path = true
 EOL
@@ -35,7 +42,7 @@ mkdir -p /etc/containerd/certs.d/quay.io
 cat >/etc/containerd/certs.d/quay.io/hosts.toml <<EOL
 server = "https://quay.io"
 
-[host."http://harbor.devops1.pbm.sh/v2/proxy-quay.io"]
+[host."${CONTAINER_REGISTRY_URL}/v2/proxy-quay.io"]
   capabilities = ["pull", "resolve"]
   override_path = true
 EOL
@@ -44,7 +51,7 @@ mkdir -p /etc/containerd/certs.d/registry.k8s.io
 cat >/etc/containerd/certs.d/registry.k8s.io/hosts.toml <<EOL
 server = "https://registry.k8s.io"
 
-[host."http://harbor.devops1.pbm.sh/v2/proxy-registry.k8s.io"]
+[host."${CONTAINER_REGISTRY_URL}/v2/proxy-registry.k8s.io"]
   capabilities = ["pull", "resolve"]
   override_path = true
 EOL
@@ -53,7 +60,7 @@ mkdir -p /etc/containerd/certs.d/registry.gitlab.com
 cat >/etc/containerd/certs.d/registry.gitlab.com/hosts.toml <<EOL
 server = "https://registry.gitlab.com"
 
-[host."http://harbor.devops1.pbm.sh/v2/proxy-registry.gitlab.com"]
+[host."${CONTAINER_REGISTRY_URL}/v2/proxy-registry.gitlab.com"]
   capabilities = ["pull", "resolve"]
   override_path = true
 EOL
